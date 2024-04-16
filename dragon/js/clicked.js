3aecce21c2f6f0b20cf51cc531281545f859462b
Cmalonzo1 <142922371+Cmalonzo1@users.noreply.github.com>
2024-04-16 10:54:29 -0500

    revamping my home page

---
+++ dragon/js/clicked.js
@@ -0,0 +1,9 @@
+/* 
+const element = document.querySelector(".list-subheader");
+
+element.addEventListener('click',function(){
+    this.classList.toggle("isClicked")
+});
+
+Toggles a class after clicking on the element listed as the querySelector parameter
+*/
